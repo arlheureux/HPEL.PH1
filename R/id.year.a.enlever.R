c4a6f536e295473ef8a459452c006dbdcf413fd4
Arnaud Lheureux <arnaudlhx@aol.com>
2024-07-30 18:07:12 +0200

    commit

--- R/id.year.a.enlever.R
+++ R/id.year.a.enlever.R
@@ -3,7 +3,7 @@
 ## Auteur : Arnaud Lheureux
 ## Email: arnaud.lheureux@sorbonne-universite.fr
 ##
-## Date de cretion : 18 juin 2024
+## Date de création : 18 juin 2024
 ##
 ## Copyright (c) Arnaud Lheureux, 2024
 ## R version : 4.4
@@ -18,12 +18,12 @@
 ## ---------------------------
 
 id.year.enlever <- function(Data_TS_month, nb.mois.min, nb.cores = detectCores()/2) {
-
+ 
   mois.non <- Data_TS_month[is.na(Data_TS_month$Val),]
   tab.mois.non <- table(mois.non$Param, mois.non$Year, mois.non$Site)
   id.non <- which(tab.mois.non > 12-nb.mois.min, arr.ind=TRUE)
-
-
+  
+  
   Year.a.enlever <- data.frame(Site = NA, Year=NA, Param=NA)
   if(nrow(id.non) != 0){
     # for(i in 1:nrow(id.non)){
@@ -31,7 +31,7 @@ id.year.enlever <- function(Data_TS_month, nb.mois.min, nb.cores = detectCores()
     #   Year.a.enlever[i,"Year"] <- colnames(tab.mois.non)[id.non[i,2]]
     #   Year.a.enlever[i,"Param"] <- rownames(tab.mois.non)[id.non[i,1]]
     # }
-
+    
     Year.a.enlever <- do.call(
       "rbind",
       pbmclapply(
@@ -52,10 +52,10 @@ id.year.enlever <- function(Data_TS_month, nb.mois.min, nb.cores = detectCores()
   }else{
     cat("Toutes les années comportent au minimum la proportion définie, et ce pour tous les paramètres\n\n")
   }
+  
+  
 
-
-
-
+  
   return(Year.a.enlever)
 }
 
@@ -71,16 +71,16 @@ id.year.enlever <- function(Data_TS_month, nb.mois.min, nb.cores = detectCores()
 #
 ## ---------------------------
 remove.years <- function(Data_TS_month, Year.a.enlever, prop.annee.mini, nb.cores = detectCores()/2) {
-
+  
   if(nrow(Year.a.enlever) == 1 & sum(is.na(Year.a.enlever)) == ncol(Year.a.enlever)) {
-
+    
     Data_TS_month_year_ok <- Data_TS_month
     Data_TS_month_year_ok$keep <- TRUE
     cat("Il n'y a rien à faire ici")
-
+    
   }else{
-
-
+    
+    
     # J'enlève les années correspondantes
     Data_TS_month$keep <- TRUE
 
@@ -92,17 +92,17 @@ remove.years <- function(Data_TS_month, Year.a.enlever, prop.annee.mini, nb.core
     for(i in unique(Data_TS_month$Serie)) {
       if (i %in% Year.a.enlever$Serie) {
         Data_TS_month[Data_TS_month$Serie == i, "keep"] <- FALSE
-      }
+      }     
     }
     # summary(Data_TS_month)
 
 
     Data_TS_month_year_ok <- Data_TS_month[Data_TS_month$keep == TRUE,]
     Prop_y_elim <- 1-table(Data_TS_month_year_ok$Param, Data_TS_month_year_ok$Site)/12/range.year
-
+    
     Prop_y_elim <- data.table::melt(Prop_y_elim)
     colnames(Prop_y_elim) <- c("Param", "Site", "value")
-
+    
     Prop_y_elim$Serie <- paste(Prop_y_elim$Site, Prop_y_elim$Param, sep = "_")
     Data_TS_month_year_ok$Serie <- paste(Data_TS_month_year_ok$Site, Data_TS_month_year_ok$Param, sep = "_")
 
@@ -113,21 +113,21 @@ remove.years <- function(Data_TS_month, Year.a.enlever, prop.annee.mini, nb.core
         unique(Data_TS_month_year_ok$Serie),
         function(p) {
           tmp <- Data_TS_month_year_ok[Data_TS_month_year_ok$Serie == p, ]
-
+          
             if (p %in% Prop_y_elim$Serie) {
               tmp[, "Prop_year_elim"] <- unname(Prop_y_elim[Prop_y_elim$Serie == p, "value"])
             }else{
               tmp[, "Prop_year_elim"] <- 0
             }
-
+          
           return(tmp)
         }
       )
     )
 
-
+    
     boxplot(unique(Data_TS_month_year_ok$Prop_year_elim), main = "Proportion des séries éliminées")
-
+    
     # J'enlève les paramètres dont on ne conserve moins de la proportion d'année limite
     Data_TS_month_year_ok <- Data_TS_month_year_ok[Data_TS_month_year_ok$Prop_year_elim < prop.annee.mini,]
   }
